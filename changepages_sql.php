5f562d11e82abd2addf9f358d670c321360eb910
pollardc001 <70393054+pollardc001@users.noreply.github.com>
2021-04-11 21:27:18 -0500

    Add files via upload

---
+++ changepages_sql.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Vet Database</title>
+</head>
+<body>
+
+	<form action="http://student01web.mssu.edu/add_sql.php" >
+          <input type="submit" value="Add Customer" />
+        </form>
+
+        <form action="http://student01web.mssu.edu/addappointment_sql.php" >
+          <input type="submit" value="Add Appointment" />
+        </form>
+
+
+</body>
+</html>
