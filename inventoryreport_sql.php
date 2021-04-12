960aaebb12eefcb891337bfe18811a690edc4992
pollardc001 <70393054+pollardc001@users.noreply.github.com>
2021-04-11 20:39:03 -0500

    Update inventoryreport_sql.php

--- inventoryreport_sql.php
+++ inventoryreport_sql.php
@@ -6,12 +6,12 @@
    <?php 
 	require ('.db.inc.php'); 
 
-	$sql = "SELECT * FROM Customer"; //Or other SQL query
+	$sql = "SELECT * FROM Inventory"; //Or other SQL query
 	$result = $conn->query($sql); //Send query to MySQL
 	if ($result->num_rows > 0) { // If we actually get output
     	   // output data of each row
     	while ($row = $result->fetch_assoc()) { //Iterate on each row, until no more data
-         echo "id: ". $row["customerId"]. " - Last Name: " . $row["lastName"]. "<br>";
+         echo "Inventory ID: ". $row["inventoryId"]. " Description: " . $row["description"]. " Price: ". $row["price"]. " Quantity: ". $row["quantity"]."<br>";
            }
 	} else {
            echo "0 results";
